268a8da7d7f958f80a9d3900df54cbdaa7536dce
hghg01 <102838081+hghg01@users.noreply.github.com>
2023-11-16 07:49:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -129,143 +129,59 @@ function animate() {
 
 // Start the animation.
 animate();
+unction texto(){
+  //elemento pego do id//
+  var num = document.getElementById("num");
+  var num2 = document.getElementById("num2");
+  //convertendo para " int "//
+  var conversao = num.value;
+  var conv2 = num2.value
+  //onde é feito a conta//
+  var juncao = parseFloat(conversao * conv2 );
+  var juncao2=parseFloat(conv2 * conversao);
+  //mostrar na pagina//
+  var body = document.getElementById("show");
+  var mostrar = " o resultado da multiplicação é " + juncao2;body.innerHTML = mostrar  
 
-
-var NUM_PARTICLES = ( ( ROWS = 300 ) * ( COLS = 600 ) ),
-    THICKNESS = Math.pow( 80, 2 ),
-    SPACING = 3,
-    MARGIN = 100,
-    COLOR = 220,
-    DRAG = 0.95,
-    EASE = 0.25,
-    
-    /*
-    
-    used for sine approximation, but Math.sin in Chrome is still fast enough :)http://jsperf.com/math-sin-vs-sine-approximation
-
-    B = 4 / Math.PI,
-    C = -4 / Math.pow( Math.PI, 2 ),
-    P = 0.225,
-
-    */
-
-    container,
-    particle,
-    canvas,
-    mouse,
-    stats,
-    list,
-    ctx,
-    tog,
-    man,
-    dx, dy,
-    mx, my,
-    d, t, f,
-    a, b,
-    i, n,
-    w, h,
-    p, s,
-    r, c
-    ;
-
-particle = {
-  vx: 0,
-  vy: 0,
-  x: 0,
-  y: 0
-};
-
-function init() {
-
-  container = document.getElementById( 'container' );
-  canvas = document.createElement( 'canvas' );
-  
-  ctx = canvas.getContext( '2d' );
-  man = false;
-  tog = true;
-  
-  list = [];
-  
-  w = canvas.width = COLS * SPACING + MARGIN * 2;
-  h = canvas.height = ROWS * SPACING + MARGIN * 2;
-  
-  container.style.marginLeft = Math.round( w * -0.5 ) + 'px';
-  container.style.marginTop = Math.round( h * -0.5 ) + 'px';
-  
-  for ( i = 0; i < NUM_PARTICLES; i++ ) {
-    
-    p = Object.create( particle );
-    p.x = p.ox = MARGIN + SPACING * ( i % COLS );
-    p.y = p.oy = MARGIN + SPACING * Math.floor( i / COLS );
-    
-    list[i] = p;
-  }
-
-  container.addEventListener( 'mousemove', function(e) {
-
-    bounds = container.getBoundingClientRect();
-    mx = e.clientX - bounds.left;
-    my = e.clientY - bounds.top;
-    man = true;
-    
-  });
-  
-  if ( typeof Stats === 'function' ) {
-    document.body.appendChild( ( stats = new Stats() ).domElement );
-  }
-  
-  container.appendChild( canvas );
 }
 
-function step() {
+function numero(){
+  var number = document.getElementById("div");
+  var num2 = document.getElementById("div2");
+  var conv = number.value;
+  var conv2 = num2.value;
+  var resultado = parseFloat(conv / conv2);
+  var resul2 = parseFloat(conv2 / conv);
+  var mostrar = document.getElementById("show2");
+  var ver = "o resultado da divisao é " + resultado;mostrar.innerHTML = ver
 
-  if ( stats ) stats.begin();
-
-  if ( tog = !tog ) {
-
-    if ( !man ) {
-
-      t = +new Date() * 0.001;
-      mx = w * 0.5 + ( Math.cos( t * 2.1 ) * Math.cos( t * 0.9 ) * w * 0.45 );
-      my = h * 0.5 + ( Math.sin( t * 3.2 ) * Math.tan( Math.sin( t * 0.8 ) ) * h * 0.45 );
-    }
-      
-    for ( i = 0; i < NUM_PARTICLES; i++ ) {
-      
-      p = list[i];
-      
-      d = ( dx = mx - p.x ) * dx + ( dy = my - p.y ) * dy;
-      f = -THICKNESS / d;
-
-      if ( d < THICKNESS ) {
-        t = Math.atan2( dy, dx );
-        p.vx += f * Math.cos(t);
-        p.vy += f * Math.sin(t);
-      }
-
-      p.x += ( p.vx *= DRAG ) + (p.ox - p.x) * EASE;
-      p.y += ( p.vy *= DRAG ) + (p.oy - p.y) * EASE;
-
-    }
-
-  } else {
-
-    b = ( a = ctx.createImageData( w, h ) ).data;
-
-    for ( i = 0; i < NUM_PARTICLES; i++ ) {
-
-      p = list[i];
-      b[n = ( ~~p.x + ( ~~p.y * w ) ) * 4] = b[n+1] = b[n+2] = COLOR, b[n+3] = 255;
-    }
-
-    ctx.putImageData( a, 0, 0 );
-  }
-
-  if ( stats ) stats.end();
-
-  requestAnimationFrame( step );
 }
 
-init();
-step();
-    
+function number(){
+  var num = document.getElementById("num2");
+  var num2 = document.getElementById("num3");
+  var rev1 = num.value;
+  var rev2 = num2.value;
+  var float1 = parseFloat (rev1);
+  console.log(float1)
+  var mostrar = document.getElementById("show3");
+  var show = "o resultado da subtração é " + float1;mostrar.innerHTML = show
+  
+}
+
+function nome(){
+ var nome = document.getElementById("name").value;
+  var h2 = document.getElementById("show4");
+ var mostr = "seja bem vindo(a)," + nome;h2.innerHTML = mostr
+  
+}
+
+function result(){
+  var numero = document.getElementById("pote");
+  var conversao = numero.value;
+  var resultado = parseFloat(conversao);
+  var final = resultado * resultado;
+  var log = document.getElementById("show5");
+  var end = "o resultado da potência é " + final;log.innerHTML = end
+
+}
